8bbef9e29e669bf5858caaad8f7efd2d0533c91d
UnLuckyCharmss <32336068+UnLuckyCharmss@users.noreply.github.com>
2018-09-17 14:04:39 +0100

    fixing greif protection (Luigi it has my paths rn)

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-set startdir=C:\Users\Domen\Desktop\buket
+set startdir=D:\BukkitServer\BukkitServer
 set bashdir="C:\Program Files\Git\git-bash.exe"
 %bashdir% --login -i -c "java -jar "%startdir%\spigot-1.8-R0.1-SNAPSHOT-latest.jar""
 pause
