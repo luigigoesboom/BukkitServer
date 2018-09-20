8444bdf069db7ae4e05ef9a7e1352d99b21e7314
luigigoesboom <42833007+luigigoesboom@users.noreply.github.com>
2018-09-20 16:54:36 +0200

    Actual server files

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-set startdir=D:\BukkitServer\BukkitServer
+set startdir=C:\Users\Domen\Desktop\buket
 set bashdir="C:\Program Files\Git\git-bash.exe"
 %bashdir% --login -i -c "java -jar "%startdir%\spigot-1.8-R0.1-SNAPSHOT-latest.jar""
 pause
